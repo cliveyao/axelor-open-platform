c574092353773a5f550beca1a934801ed17ed302
cliveyao <clive.yao@gmail.com>
2020-11-05 05:07:36 -0800

    sql server

--- axelor-core/src/main/java/com/axelor/db/hibernate/dialect/unique/SqlServer2008UniqueDelegate.java
+++ axelor-core/src/main/java/com/axelor/db/hibernate/dialect/unique/SqlServer2008UniqueDelegate.java
@@ -27,24 +27,6 @@ public class SqlServer2008UniqueDelegate extends DefaultUniqueDelegate {
 
 	// legacy model ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	protected String uniqueConstraintSql1(UniqueKey uniqueKey) {
-		final StringBuilder sb = new StringBuilder();
-		sb.append( "unique (" );
-		final Iterator<org.hibernate.mapping.Column> columnIterator = uniqueKey.columnIterator();
-		while ( columnIterator.hasNext() ) {
-			final org.hibernate.mapping.Column column = columnIterator.next();
-			sb.append( column.getQuotedName( dialect ) );
-			if ( uniqueKey.getColumnOrderMap().containsKey( column ) ) {
-				sb.append( " " ).append( uniqueKey.getColumnOrderMap().get( column ) );
-			}
-			if ( columnIterator.hasNext() ) {
-				sb.append( ", " );
-			}
-		}
-
-		return sb.append( ')' ).toString();
-	}
-
 	protected String uniqueConstraintSql(UniqueKey uniqueKey) {
 		final StringBuilder sb = new StringBuilder();
 		final StringBuilder whereIsNotNull = new StringBuilder();
